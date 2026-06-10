7968a317abb6a1d4ac5a872ca2e87ec8229d466d
Phat Nguyen Khac <ngkhacphat2006@gmail.com>
2026-06-10 14:20:05 +0700

    Hoàn thành components Header

---
+++ reactjs/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header-banner text-white p-4 rounded mb-4 shadow-sm">
+            <h2>Quản lý danh sách sản phẩm</h2>
+            <p className="mb-0">Giao diện nhập liệu và hiển thị danh sách sản phẩm một cách trực quan, rõ ràng.</p>
+        </div>
+    );
+}
+
+export default Header;
